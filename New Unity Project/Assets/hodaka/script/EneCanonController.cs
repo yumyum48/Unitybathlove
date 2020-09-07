6a2b770a87238fd83b8ff918ce2d7f7cd7937667
akado_ran <nisihuni1234@gmail.com>
2020-09-07 12:27:13 +0900

    ボールの処理をいろいろいい感じに書きました

---
+++ New Unity Project/Assets/hodaka/script/EneCanonController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine;
+
+public class EneCanonController : MonoBehaviour
+{
+    public GameObject muzzlePoint;
+    public GameObject ball;
+    public float speed = 30f;
+    private bool ballFlg = true;
+    private float frameCount = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ballFlg == true)
+        {
+            EneCanonShot();
+            ballFlg = false;
+        }
+        if(frameCount++ >= 300)
+        {
+            ballFlg = true;
+            frameCount = 0;
+        }
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Finish")
+        {
+            Destroy(gameObject, 0.2f);
+        }
+    }
+
+    public void EneCanonShot()
+    {
+        Vector3 mballPos = muzzlePoint.transform.position;
+        GameObject newball = Instantiate(ball, mballPos, transform.rotation);
+        Vector3 dir = newball.transform.forward;
+        newball.GetComponent<Rigidbody>().AddForce(dir * speed, ForceMode.Impulse);
+        newball.name = ball.name;
+    }
+
+}
---
+++ New Unity Project/Assets/hodaka/script/Enemyscript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemyscript : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Finish")
+        {
+            Destroy(gameObject, 0.2f);
+        }
+    }
+}
