94cb2b1512d08a18badb99b78558cbd14c61b273
yumayuma48 <puxipu939@gmail.com>
2020-09-14 10:49:36 +0900

    タイトル画面からゲームメイン画面に行けるようにした

---
+++ New Unity Project/Assets/yumas/Script/SceneChange.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;  // シーンマネジメントの追加
+
+public class SceneChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown("joystick button 1")) //マウス左クリック、スペースキー、Aボタン、ジャンプボタンを押した場合
+        {
+            SceneManager.LoadScene("GameMain");//some_senseiシーンをロードする
+        }
+    }
+}
