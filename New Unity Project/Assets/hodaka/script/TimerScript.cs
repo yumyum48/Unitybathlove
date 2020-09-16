99f9b3955efbf021edee9f6d0ec4e22078c7431d
akado_ran <nisihuni1234@gmail.com>
2020-09-16 18:21:22 +0900

    時間+クリア画面の仮表示

---
+++ New Unity Project/Assets/hodaka/script/TimerScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+    public float  TimeSeconds = 60.0f;//タイマー表示用
+    int TIME;
+    public bool TimeEndFlg = false;//タイマーが終了したか管理するフラグ
+    private Text textTimer;
+    public Text textGameOver;
+    public Text textGameClear;
+
+    void Start()
+    {
+        textTimer = GameObject.Find("Timer").GetComponent<Text>();
+        TimeSeconds += 1;//(x.00からではなくx.99から開始させるため　ただの見栄え）
+        textGameOver.enabled = false; //開始段階では存在を抹消する
+        textGameClear.enabled = false;//同じく
+    }
+    void Update()
+    {
+        TimeSeconds -= Time.deltaTime;
+        TIME = (int)TimeSeconds;
+        if (TimeSeconds <= 0.0f)
+        {
+            TimeSeconds = 0.0f;
+            TimeEndFlg = true;
+        }
+        textTimer.text = "TIME : " + TIME.ToString();
+        GameOverDrawing();
+
+
+    }
+    public void GameOverDrawing() {
+        if (TimeEndFlg == true)
+        {
+            textGameClear.enabled = true;
+        }
+    }
+}
